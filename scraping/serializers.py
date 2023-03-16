214629be65bd4d0a785c7a63364e43b6c8fa6f9e
h4med <hamed.d@gmail.com>
2023-03-16 20:09:18 +0330

    add url to serializer

--- scraping/serializers.py
+++ scraping/serializers.py
@@ -32,3 +32,29 @@ class TweetListSerializer(serializers.ModelSerializer):
             'created_at',
             'tweet_id',
         )
+
+class TweetDetailSerializer(serializers.ModelSerializer):
+    tweet_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Tweet
+        # fields = '__all__'
+        fields = (
+            'handle',            
+            'publish_date',
+            'text',
+            'sentiment',
+            'tweet_url',
+            'image_url',
+            'video_url',
+            'created_at',
+            'tweet_id',
+            'likes',
+            'retweets',
+            'replies',
+            'views',
+        )
+    
+    def get_tweet_url(self, obj):
+        url = "https://twitter.com/"+obj.handle[1:]+"/status/"+obj.tweet_id
+        return url
