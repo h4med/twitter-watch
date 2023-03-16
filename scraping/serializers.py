378d1ea013f08e92dd47ecb1146d7ad38764f791
h4med <hamed.d@gmail.com>
2023-03-16 20:17:59 +0330

    add url to serializers

--- scraping/serializers.py
+++ scraping/serializers.py
@@ -1,26 +1,8 @@
 from rest_framework import serializers
 from .models import Tweet
 
-class TweetDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tweet
-        # fields = '__all__'
-        fields = (
-            'handle',            
-            'publish_date',
-            'text',
-            'sentiment',
-            'image_url',
-            'video_url',
-            'created_at',
-            'tweet_id',
-            'likes',
-            'retweets',
-            'replies',
-            'views',
-        )
-
 class TweetListSerializer(serializers.ModelSerializer):
+    tweet_url = serializers.SerializerMethodField()
     class Meta:
         model = Tweet
         # fields = '__all__'
@@ -31,9 +13,14 @@ class TweetListSerializer(serializers.ModelSerializer):
             'sentiment',
             'created_at',
             'tweet_id',
+            'tweet_url',
         )
 
-class TweetDetailSerializerWithURL(serializers.ModelSerializer):
+    def get_tweet_url(self, obj):
+        url = "https://twitter.com/"+obj.handle[1:]+"/status/"+str(obj.tweet_id)
+        return url        
+
+class TweetDetailSerializer(serializers.ModelSerializer):
     tweet_url = serializers.SerializerMethodField()
 
     class Meta:
